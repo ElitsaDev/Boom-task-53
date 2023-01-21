380a1e90a98fed3f2e0b6666364fa522f091da6d
ElitsaDev <94979851+ElitsaDev@users.noreply.github.com>
2023-01-21 19:43:33 +0200

    dynamically added attributes to products

--- src/js/app.js
+++ src/js/app.js
@@ -2,6 +2,9 @@ import "../scss/app.scss";
 
 window.addEventListener("DOMContentLoaded", () => {
   // This block will be executed once the page is loaded and ready
-
-
+    let products = document.getElementsByClassName('product');
+    Array.from(products).forEach(product => {
+        let value = document.querySelector('.price').textContent;
+        product.setAttribute('data-price', value);
+    });
 });
